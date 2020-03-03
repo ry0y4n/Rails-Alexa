4d91fedfdd7126c6e876c8b43e8e78fa74ab0fd5
ry0y4n <rusk.ryoy4n@gmail.com>
2020-03-03 22:12:19 +0900

    can't verify CSRF token authenticity

--- app/controllers/talk_controller.rb
+++ app/controllers/talk_controller.rb
@@ -1,5 +1,5 @@
 class TalkController < ApplicationController
-    protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
 
   def create
     request = AlexaRubykit::build_request(params)
