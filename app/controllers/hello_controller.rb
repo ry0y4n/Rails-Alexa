621d6902da2e4ce5026231e0891ea0b0e89000af
ry0y4n <rusk.ryoy4n@gmail.com>
2020-03-03 19:23:19 +0900

    first commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,20 @@
+class HelloController < ApplicationController
+
+    def index
+        if params['request'] != nil then
+            @result = 'Request'
+        else
+            @result = ''
+        end
+        
+        if request.post? then
+            @title = 'Result'
+            @msg = 'you typed: ' + params['input1'] + '.'
+            @value = params['input1']
+        else
+            @title = 'Index'
+            @msg = 'type text...'
+            @value = ''
+        end
+    end
+end
