93043326e00534e29bee02982b8de89b02739c48
ry0y4n <rusk.ryoy4n@gmail.com>
2020-03-03 21:55:05 +0900

    logger

--- app/controllers/talk_controller.rb
+++ app/controllers/talk_controller.rb
@@ -2,9 +2,6 @@ class TalkController < ApplicationController
     protect_from_forgery with: :null_session
 
   def create
-    request = AlexaRubykit::build_request(params)
-    response = AlexaRubykit::Response.new
-    response.add_speech("あなたの名前は#{request.slots[:MyName][:value]}ですね")
-    render json: response.build_response
+    Rails.logger.info params
   end
 end
