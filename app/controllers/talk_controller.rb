9925ca87d314dd47ae06a5eb7e7558880665dd0f
ry0y4n <rusk.ryoy4n@gmail.com>
2020-03-03 22:19:31 +0900

    Request

--- app/controllers/talk_controller.rb
+++ app/controllers/talk_controller.rb
@@ -2,7 +2,7 @@ class TalkController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    request = AlexaRubykit::build_request(params)
+    request = AlexaRubykit::Request.build_request(params)
     response = AlexaRubykit::Response.new
     response.add_speech("あなたの名前は#{request.slots[:MyName][:value]}ですね")
     render json: response.build_response
